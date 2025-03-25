18404761cf236582244cdf78f2dba087b50c8fec
liviasab <liviasabino92@gmail.com>
2025-03-25 11:18:40 -0300

    feat: created the project

---
+++ src/test/java/sabino/livia/WorkoutLog2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package sabino.livia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutLog2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
